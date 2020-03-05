f250b6d291710af4bb0d81b8d39058e8376a7566
Libb521 <eotieno39@yahoo.com>
2020-03-05 13:30:06 +0300

    changed from the text field to charfield in the models file

--- gram/models.py
+++ gram/models.py
@@ -5,16 +5,16 @@ from django.contrib.auth.models import User
 
 class image(models.Model):
     image= models.ImageField(blank=True, null=True)
-    image_name= models.TextField(max_length=50)
-    image_caption= models.TextField(max_length=100)
+    image_name= models.CharField(max_length=50)
+    image_caption= models.CharField(max_length=100)
     profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
     likes = models.PositiveIntegerField('Likes', blank=False, default=0)
     comments= models.TextField(max_length=150)
      # likes=
 
 class Profile(models.Model):
-    first_name = models.TextField(max_length=50)
-    last_name = models.TextField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     profile_picture = models.ImageField(upload_to='prof_images')
     bio = models.TextField(max_length=120)
 
