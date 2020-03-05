1e3400343345d274cec4328ef9eeb9aa8cec602e
Libb521 <eotieno39@yahoo.com>
2020-03-05 10:45:43 +0300

    created the images class model and created a superuser

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class image(models.Model):
+    image= models.ImageField(blank=True, null=True)
+    image-name= models.TextField(max_length=70)
+    image-caption= models.TextField(max_length=150)
+    profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    # likes=
+    # comments=
