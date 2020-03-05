46cd0bc52cec6f288e9e75aed480f64d910c5dcb
Libb521 <eotieno39@yahoo.com>
2020-03-05 11:16:43 +0300

    rectified an error in the fields within the image class, i replaced a hyphen with an underscore

--- gram/models.py
+++ gram/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class image(models.Model):
-    image= models.ImageField(blank=True, null=True)
-    image-name= models.TextField(max_length=70)
-    image-caption= models.TextField(max_length=150)
+    image= models.ImageField(blank=True, null=True),
+    image_name= models.TextField(max_length=70)
+    image_caption= models.TextField(max_length=150)
     profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
     # likes=
     # comments=
