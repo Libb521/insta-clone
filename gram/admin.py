6d04f3c31ee6fef23fe522ba5bcf134758c45c22
Libb521 <eotieno39@yahoo.com>
2020-03-05 13:27:23 +0300

    imported Profile model into the admin file and registered it

--- gram/admin.py
+++ gram/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import image
+from .models import image, Profile
 
 # Register your models here.
 admin.site.register(image)
+admin.site.register(Profile)
