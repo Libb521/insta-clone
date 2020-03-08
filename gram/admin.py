529477ed87723f7ecd404a319ee0256ef82df8cb
Libb521 <eotieno39@yahoo.com>
2020-03-08 16:35:38 +0300

    created a superuser for my admin page

--- gram/admin.py
+++ gram/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import image, Profile
+from .models import image, Profile, Comment, Likes
 
 # Register your models here.
 admin.site.register(image)
 admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Likes)
--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import image, Profile, Comment, Likes
 
 # Create your tests here.
+class imageTestClass(TestCase):
+    def setUp(self):
+        self.
