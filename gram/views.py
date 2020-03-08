e9f70dc7fb8e0e5ac9b75afcc49db95f138d974b
Libb521 <eotieno39@yahoo.com>
2020-03-08 13:22:05 +0300

    defined the profile function

--- gram/views.py
+++ gram/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def profile(request):
+    current_user = request.current_user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.owner = current_user
+            profile.save()
+    else:
+        form=ProfileForm()
+    return render(request, 'concept/new.html', locals())
