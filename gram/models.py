f8e6ad7547a2f25d9d3166eadbf13b8d3e70def5
Libb521 <eotieno39@yahoo.com>
2020-03-08 15:57:37 +0300

    added two more model fields  'comment and likes'

--- gram/models.py
+++ gram/models.py
@@ -8,7 +8,7 @@ class image(models.Model):
     image_name= models.CharField(max_length=50)
     image_caption= models.CharField(max_length=100)
     profile= models.ForeignKey('auth.user',on_delete=models.CASCADE)
-    likes = models.PositiveIntegerField('Likes', blank=False, default=0)
+    
     comments= models.TextField(max_length=150)
 
     def __str__(self):
@@ -33,6 +33,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.bio)
+    class Meta:
+
+        ordering = ['bio']
 
     def profile_save(self):
         self.save()
@@ -44,4 +47,24 @@ class Profile(models.Model):
     def get_profile_by_username(cls, owner):
         profiles = cls.object.filter(owner_contains=owner)
         return profiles
-    
+
+class Comment(models.Model):
+    image =  models.ForeignKey(image, blank=True, on_delete=models.CASCADE, related_name='comment')
+    commenter = models.CharField(max_length=100)
+    comment = models.TextField()
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        delete.comment()
+    @classmethod
+    def get_profile_comments(cls, profile):
+        comments = comment.objects.filter(image_pk=id)
+        return comments
+    def __str__(self):
+        return str(str.comment)
+
+class Likes(models.Model):
+    likes = models.PositiveIntegerField('Likes', blank=False, default=0)
+    image =  models.ForeignKey(image, blank=True, on_delete=models.CASCADE, related_name='likes') 
