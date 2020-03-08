ab35208ed7e5a9c81e315fe7f1e1c8953c1d9dc6
Libb521 <eotieno39@yahoo.com>
2020-03-08 21:04:50 +0300

    rectified all the files that had the likes model

--- gram/tests.py
+++ gram/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import image, Profile, Comment, Likes
+from .models import image, Profile, Comment
 
 # Create your tests here.
 class imageTestClass(TestCase):
--- gram/views.py
+++ gram/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.conf import settings
+from django.urls import path, include
+from django.contrib.auth import authenticate, login, logout
+# from .forms import PostForm
+from .models import image, Profile, Comment
+from . import models
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def welcome(request):
