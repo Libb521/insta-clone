ef465f16052afdddeed2ea9609fcee38822b69ac
Libb521 <eotieno39@yahoo.com>
2020-03-09 11:00:26 +0300

    created the add_image, home,search and display_profile views and linked to the Urls

--- gram/urls.py
+++ gram/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     path('',views.welcome,name ='welcome'),
     path('newprofile/',views.profile,name = 'profile'),
+    path('image/',views.add_image,name = 'upload_image'),
+    path('search/',views.search,name = 'search'),
+    path('showprofile/',views.display_profile,name = 'showprofile'),
     ]
--- gram/views.py
+++ gram/views.py
@@ -26,3 +26,51 @@ def profile(request):
     else:
         form=ProfileForm()
     return render(request, 'concept/new.html', locals())
+
+def add_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.profile = current_user
+            add.save()
+            return redirect('home')
+    else:
+        form = ImageForm()
+
+
+    return render(request,'concept/image.html',locals())
+
+def home(request):
+    current_user = request.user
+    all_images = Image.objects.all()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
+
+def search(request):
+    profiles = User.objects.all()
+
+    if 'username' in request.GET and request.GET['username']:
+        search_term = request.GET.get('username')
+        results = User.objects.filter(username__icontains=search_term)
+        print(results)
+
+        return render(request,'concept/results.html',locals())
+
+    return redirect(home)
+
+def display_profile(request, id):
+    seekuser=User.objects.filter(id=id).first()
+    profile = seekuser.profile
+    profile_details = Profile.get_by_id(id)
+    images = Image.get_profile_images(id)
+
+    usersss = User.objects.get(id=id)
+    people=User.objects.all()
+    
+
+    return render(request,'concept/profile.html',locals())
