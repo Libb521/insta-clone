3467159b8dbceb0ee4ed43d7069ca66fba63c43b
Libb521 <eotieno39@yahoo.com>
2020-03-09 08:13:31 +0300

    rectified some fields in the manage.py file so that migrations can happen

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+if __name__ == '__main__':
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
     try:
@@ -17,5 +17,4 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
-    main()
+
